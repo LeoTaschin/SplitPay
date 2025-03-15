6be56bf9105df4c5b7b24ac40c2477c371d867ca
Leonardo Taschin <taschin21@gmail.com>
2025-03-15 18:14:28 -0300

    fix: navigation and gesture handler configuration

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from './src/context/ThemeContext';
 import AppNavigator from './src/navigation/AppNavigator';
+import { View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AppNavigator />
-    </ThemeProvider>
+    <View style={styles.container}>
+      <ThemeProvider>
+        <AppNavigator />
+      </ThemeProvider>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
